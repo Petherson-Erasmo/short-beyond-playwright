d55a3e964d99fa2bd043fa372ec6a1bfa6198baa
Petherson Erasmo <petherson_erasmo@hotmail.com>
2025-10-12 13:00:09 -0300

    nona aula

--- playwright/support/services/links.js
+++ playwright/support/services/links.js
@@ -8,7 +8,16 @@ export const linksService = (request) => {
         })
     }
 
+    const getLinks = async (token) => {
+        return await request.get('http://localhost:3333/api/links', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    }
+
     return {
-        createLink
+        createLink,
+        getLinks
     }
 }
